fa2f71e3a3b319af11f6e955c3b260ccf7842b80
BayajidAlam <bayajidalam2001@gmail.com>
2024-01-01 14:08:30 +0600

    starting problem solving

---
+++ codeforces/A_Watermelon.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+  int wt;
+  cin >> wt;
+  if (wt % 2 == 0 && wt > 2)
+  {
+    cout << "YES" << endl;
+  }
+  else
+  {
+    cout << "NO" << endl;
+  }
+  return 0;
+}
