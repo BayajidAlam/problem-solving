734b0a8593844a43615138eb0380b5761a130206
BayajidAlam <bayajidalam2001@gmail.com>
2024-01-08 23:25:06 +0600

    day challenge: 08-01-24 done

---
+++ leetcode/Max_product_of_3_number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int sz = nums.size();
+        
+        sort(nums.begin(),nums.end());
+
+        return max(nums[0]*nums[1] * nums[sz-1], nums[sz-1]*nums[sz-2]*nums[sz-3]);
+    }
+};
