416340c6afa9b9b1ba877a6b6458f5a4c8638135
BayajidAlam <bayajidalam2001@gmail.com>
2024-02-27 23:59:59 +0600

    day challenge: 27:02:24 done

---
+++ B_Fair_Division.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    int div = 0, sum = 0, one = 0;
+    for (int i = 0; i < n; i++)
+    {
+      int x;
+      cin >> x;
+      if (x == 1)
+      {
+        one++;
+        sum++;
+      }
+      else
+        sum += 2;
+    }
+
+    if (sum % 2 == 0)
+    {
+      div = sum / 2;
+      if (div % 2 == 0)
+      {
+        cout << "YES" << endl;
+      }
+      else
+      {
+        if (one)
+          cout << "YES" << endl;
+        else
+          cout << "NO" << endl;
+      }
+    }
+    else
+    {
+      cout << "NO" << endl;
+    }
+  }
+  return 0;
+}
